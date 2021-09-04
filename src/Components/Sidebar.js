adee998651cc400a0fdb4070bb8edfd822282c6a
Aakash <tiwaryaakash00@gmail.com>
2021-09-04 21:29:58 +0530

    created chat rooms

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import DashBoardToggle from './dashboard/DashboardToggle'
-
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal'
 const Sidebar = () => {
   return (
     <div className = "h-100 pt-2">
 
        <div>
          <DashBoardToggle />  
+         <CreateRoomBtnModal></CreateRoomBtnModal>
        </div>  
        
        bottom 
